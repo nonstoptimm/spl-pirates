f4876e12b02bfc2d063a48ccc5e1e46bc2e15e69
Ali0807 <albert.thieme@gmx.de>
2018-01-20 14:44:25 +0100

    Identifying Treatment Group

---
+++ Identifying Treatment and Control Group.R
@@ -0,0 +1,12 @@
+##### Identifying Treatment and Control Groups #####
+
+##Treamtment group had a hourly wage less then 8.50€ before 2015 and are affected of minimum wage according to survey2015
+
+#For merged2015 use variable 'Collective Wage Agreement Minimum Wage#
+# Treatment <- merged2015$'Collective Wage Agreement Minimum Wage'
+
+Treatment <- 
+
+attributes(merged2015$`Collective Wage Agreement Minimum Wage`)
+summary(merged2015$`Collective Wage Agreement Minimum Wage`)
+summary(merged2015$`Amount of Education Or Training in Years`)
