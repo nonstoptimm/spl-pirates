8c6fc651111b644ce5ad6fcf9ee37994c2aebbe1
Jupp Kerschek <juppkerschek@Test1.fritz.box>
2018-03-10 15:57:17 +0100

    Graphs for timetrend Kaitz

---
+++ graphs for Kaitz over time.R
@@ -0,0 +1,31 @@
+#Generate graphic for kaitz over time
+
+# install.packages("ggplot2")
+library("ggplot2")
+
+
+ggplot(data = dbys, aes(x= Wave, y = Kaitz, color = State.of.Residence, group = State.of.Residence)) +
+  geom_line() +
+  theme(panel.background = element_rect(fill = "white")) +
+  labs(title = "Kaitz Index over Years",
+       y = "Kaitz-Index",
+       x = "Years") +
+  geom_vline(xintercept = 6, color = "red") +
+  theme_classic() +
+  scale_colour_hue(name = "States",
+                      labels = c("Schleswig-Holstein", 
+                                 "Hamburg", 
+                                 "Lower Saxony", 
+                                 "Bremen", 
+                                 "North-RhineWestfalia", 
+                                 "Hessen", 
+                                 "Rheinland-Pfalz", 
+                                 "Baden-Wuerttemberg", 
+                                 "Bavaria", 
+                                 "Saarland", 
+                                 "Berlin", 
+                                 "Brandenburg", 
+                                 "Mecklemburg-Vorpommern", 
+                                 "Saxony", 
+                                 "Saxony-Anhalt", 
+                                 "Thuringia")) 
