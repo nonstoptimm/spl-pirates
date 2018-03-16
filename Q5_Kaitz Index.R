b58e8544b7816789aca5f4022a1a8b567d70b242
torsion214 <t_walz@gmx.net>
2018-03-16 15:33:16 +0100

    Ghosts'n'Stuff Corrected and Improved, especially Q5

--- Q5_Kaitz Index.R
+++ Q5_Kaitz Index.R
@@ -3,7 +3,6 @@
 # The Fraction Index is the ratio of affected individuals by the minimum wage, hence all that earn less than 8.50 Euro per hour.
 # Kaitz Index is ration between the minimum legal wage and the average wage
 
-
 #Note Legend according to SOEP Info:
 # https://data.soep.de/soep-core
 # -1: no answer /don`t know
@@ -124,23 +123,35 @@ list_years_up = list_years + 1
 # Create Year Periods by pasting list_years and list_years_up together with a "/"-separator and assign it to Correlation.Bites.yearly$Period
 Correlation.Bites.yearly$Period = paste(list_years, list_years_up, sep = "/")
 
-
-ggplot(data = Correlation.Bites.yearly, aes(x = Period, group = Correlation.Fraction.Kaitz))+
+# Plot Function for Correlation of Bites (Yearly)
+plot_correlation_bites_yearly = function(x) {
+ggplot(data = x, aes(x = Period, group = Correlation.Fraction.Kaitz))+
   geom_bar(aes(y = Correlation.Fraction.Kaitz), stat = "identity") + 
   theme_classic() +
   labs(title = "Correlation of Bites",
        y = "Correlation",
        x = "Years") +
   coord_cartesian(ylim = c(0.00,1)) 
+}
 
+# Apply plot_correlation_bites_yearly to create bar chart for the states
+plot_output_correlation_bites_yearly = plot_correlation_bites_yearly(Correlation.Bites.yearly)
+# Save the plot_output_correlation_bites_yearly
+# ggsave("plots/plot_output_correlation_bites_yearly.png", plot_output_correlation_bites_yearly)
 
-Correlation.Bites.State = dbys %>%
+# Create function to summarize correlation between Fraction and Kaitz
+summarize_corr_fk = function(x) {
+Correlation.Bites.State = x %>%
   group_by(State.of.Residence) %>%
-  summarise(Correlation.Fraction.Kaitz = cor(Fraction, Kaitz, use ="all.obs", method="pearson" ))
+  summarise(Correlation.Fraction.Kaitz = cor(Fraction, Kaitz, use ="all.obs", method="pearson"))
+}
 
+# Apply summarize_corr_fk to dbys dataset
+Correlation.Bites.State = summarize_corr_fk(dbys)
 
-# table 
-ggplot(data = Correlation.Bites.State, aes(x = State.of.Residence, group = Correlation.Fraction.Kaitz, fill = State.of.Residence))+
+# Plot Function for Correlation of Bites (States)
+plot_correlation_bites_states = function(input) {
+  ggplot(data = input, aes(x = State.of.Residence, group = Correlation.Fraction.Kaitz, fill = State.of.Residence))+
   geom_bar(aes(y = Correlation.Fraction.Kaitz), stat = "identity" ) + 
   theme_classic() +
   labs(title = "Correlation of Bites",
@@ -165,38 +176,72 @@ ggplot(data = Correlation.Bites.State, aes(x = State.of.Residence, group = Corre
                             "Saxony", 
                             "Saxony-Anhalt", 
                             "Thuringia")) 
+}
 
+# Apply plot_correlation_bites_states to create bar chart for the states
+plot_output_correlation_bites_states = plot_correlation_bites_states(Correlation.Bites.State)
+# Save the plot_output_correlation_bites
+# ggsave("plots/plot_output_correlation_bites_states.png", plot_output_correlation_bites_states)
 
 ### OUTPUT FRACTION and KEITZ  ###
 
-##Density Plots of Fraction Index with aggreagted Data
-ggplot(data = dbys, aes(x = Fraction, group = Wave, color = Wave )) +
-  geom_line(stat = "density") + 
-  theme_classic() +
-  scale_colour_hue(name = "Years") +
-  labs(title = "Density of the Fraction Index of States seperated by Years ",
-       y = "Density",
-       x = "Fraction") +
-  coord_cartesian(xlim = c(0.1,0.6))
+##Density Plots of Kaitz or Fraction Index with aggreagted data
+plot_density_aggregated = function(input, index, xlim) {
+  if(index == "Kaitz") {
+    input$x = input$Kaitz
+  } else if(index == "Fraction") {
+    input$x = input$Fraction
+  } else {
+    print("Index must be either Kaitz or Fraction!")
+  }
+  ggplot(data = input, aes(x = x, group = Wave, color = Wave )) +
+    geom_line(stat = "density") + 
+    theme_classic() +
+    scale_colour_hue(name = "Years") +
+    labs(title = paste(index, "Index-Density of States seperated by Years", sep="-"),
+         y = "Density",
+         x = index) +
+    coord_cartesian(xlim = xlim)
+}
 
-# Function to test normality assumption of fraction
-shapiro_test = function(input, list_years) {
+# Apply plot_density_aggregated to create density plot for Fraction
+plot_density_aggr_fraction = plot_density_aggregated(dbys, "Fraction", c(0.1,0.6))
+# Save the plot_density_aggr_fraction
+# ggsave("plots/plot_aggregated_data_fraction.png", plot_aggregated_data_fraction)
+
+# Function to test normality assumption of fraction or kaitz
+shapiro_test = function(input, mode, list_years) {
+  if(mode == "Fraction") {
+    input$mode = input$Fraction
+  } else if (mode == "Kaitz") {
+    input$mode = input$Kaitz
+  } else {
+    print("Mode must be either Fraction or Kaitz!")
+  }
   for(years in 1:length(list_years)) {
-  test = shapiro.test(input$Fraction[input$Wave==list_years[years]])  ## for each year and table this loop
+  test = shapiro.test(input$mode[input$Wave==list_years[years]])  ## for each year and table this loop
   print(list_years[years])
   print(test)
   }
 }
 
-# Apply shapiro_test-function to dbys using the years in list_years
-shapiro_test(dbys, list_years)
+# Apply shapiro_test-function to dbys using the years in list_years and fraction index
+shapiro_test(dbys, "Fraction", list_years)
   
-##Fraction Indexes over time with aggregated data
-ggplot(data = dbys, aes(x= Wave, y = Fraction, color = State.of.Residence, group = State.of.Residence)) +
+##Plot-Function for Kaitz or Fraction Indexes over time with aggregated data
+plot_aggregated_data = function(input, index) {
+  if(index == "Kaitz") {
+    input$y = input$Kaitz
+  } else if(index == "Fraction") {
+    input$y = input$Fraction
+  } else {
+    print("Index must be either Kaitz or Fraction!")
+  }
+  ggplot(data = input, aes(x= Wave, y = Fraction, color = State.of.Residence, group = State.of.Residence)) +
   geom_line() +
   theme(panel.background = element_rect(fill = "white")) +
   labs(title = "Fraction Index over Years",
-       y = "Fraction-Index",
+       y = paste(index, "Index", sep="-"),
        x = "Years") +
   geom_vline(xintercept = 5, color = "red") +
   theme_classic() +
@@ -217,61 +262,31 @@ ggplot(data = dbys, aes(x= Wave, y = Fraction, color = State.of.Residence, group
                               "Mecklemburg-Vorpommern", 
                               "Saxony", 
                               "Saxony-Anhalt", 
-                              "Thuringia")) 
-
-
-##Density Plots of Kaitz Index with aggreagted Data
-ggplot(data = dbys, aes(x = Kaitz, group = Wave, color = Wave )) +
-  geom_line(stat = "density") + 
-  theme_classic() +
-  scale_colour_hue(name = "Years") +
-  labs(title = "Density of the Kaitz Index of States seperated by Years ",
-       y = "Density",
-       x = "Kaitz") +
-  coord_cartesian(xlim = c(0.43,0.7))
-
-#Test normality assumption of Kaitz
-shapiro_test = function(input, list_years) {
-  i = 1
-  for(years in list_years) {
-    test = shapiro.test(input$Kaitz[input$Wave==list_years[i]])  ## for each year and table this loop
-    print(list_years[i])
-    print(test)
-    i = i + 1
-  }
+                              "Thuringia"))
 }
 
-# Apply Shapiro Test to dbys using shapiro_test and the list of years
-shapiro_test(dbys, list_years)
+# Apply the plot_aggregated_data for Fraction index
+plot_aggregated_data_fraction = plot_aggregated_data(dbys, "Fraction")
+# Save the plot_aggregated_data_fraction
+# ggsave("plots/plot_aggregated_data_fraction.png", plot_aggregated_data_fraction)
 
-##Kaitz Indexes over time with aggregated Data
-ggplot(data = dbys, aes(x= Wave, y = Kaitz, color = State.of.Residence, group = State.of.Residence)) +
-  geom_line() +
-  theme(panel.background = element_rect(fill = "white")) +
-  labs(title = "Kaitz Index over Years",
-       y = "Kaitz-Index",
-       x = "Years") +
-  geom_vline(xintercept = 5, color = "red") +
-  theme_classic() +
-  coord_cartesian(xlim = c(1.6,7)) +
-  scale_colour_hue(name = "States",
-                   labels = c("Schleswig-Holstein", 
-                              "Hamburg", 
-                              "Lower Saxony", 
-                              "Bremen", 
-                              "North-RhineWestfalia", 
-                              "Hessen", 
-                              "Rheinland-Pfalz", 
-                              "Baden-Wuerttemberg", 
-                              "Bavaria", 
-                              "Saarland", 
-                              "Berlin", 
-                              "Brandenburg", 
-                              "Mecklemburg-Vorpommern", 
-                              "Saxony", 
-                              "Saxony-Anhalt", 
-                              "Thuringia")) 
+# Apply the plot_aggregated_data for Kaitz index
+plot_aggregated_data_kaitz = plot_aggregated_data(dbys, "Kaitz")
+# Save the plot_aggregated_data_kaitz
+# ggsave("plots/plot_aggregated_data_kaitz.png", plot_aggregated_data_kaitz)
 
+# Apply plot_density_aggregated to create density plot for Kaitz
+plot_density_aggr_kaitz = plot_density_aggregated(dbys, "Kaitz", c(0.43,0.7))
+# Save the plot_density_aggr_kaitz
+# ggsave("plots/plot_density_aggr_kaitz.png", plot_density_aggr_kaitz)
+
+# Apply Shapiro Test to dbys using shapiro_test and the list of years for kaitz index
+shapiro_test(dbys, "Kaitz", list_years)
+
+# Apply the plot_aggregated_data for Kaitz index
+plot_aggregated_data_kaitz = plot_aggregated_data(dbys, "Kaitz")
+# Save the plot_aggregated_data_kaitz
+# ggsave("plots/plot_aggregated_data_kaitz.png", plot_aggregated_data_kaitz)
 
 ### Correlations between Bites
 
