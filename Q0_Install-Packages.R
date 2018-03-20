7c257e7360437eb26ae9a7f6fea369cceeac4cd1
torsion214 <t_walz@gmx.net>
2018-03-20 16:34:35 +0100

    Test Function Updated

--- Q0_Install-Packages.R
+++ Q0_Install-Packages.R
@@ -1,6 +1,6 @@
 ### INSTALL ALL NECESSARY, ADDITIONAL PACKAGES ###
 # Vector with names of all packages
-list_packages = c("foreign", "stringr", "data.table", "stargazer", "dplyr", "sp")
+list_packages = c("foreign", "stringr", "data.table", "stargazer", "dplyr", "sp", "plm")
 # lapply on the package vector
 lapply(list_packages, install.packages)
 # Delete the package vector again
