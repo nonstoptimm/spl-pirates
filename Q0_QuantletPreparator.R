3c0b4b6abaf1b86b0d6ccdaf3dff3466276d0880
torsion214 <t_walz@gmx.net>
2018-03-21 13:12:32 +0100

    QuantletPreparation

--- Q0_QuantletPreparator.R
+++ Q0_QuantletPreparator.R
@@ -13,6 +13,26 @@ tidy_source("Q6_Treatment-Graphs.R", file = "SOEPQ6/TreatmentGraphs.R")
 tidy_source("Q7_DiD_Estimation.R", file = "SOEPQ7/DiDEstimation.R")
 tidy_source("Q8_Statistical_Tests.R", file = "SOEPQ8/StatisticalTests.R")
 
+# needed only for package installation or update
+library(devtools)
+devtools::install_github("lborke/yamldebugger")
+
+# load the package every time you want to use 'yamldebugger'
+library(yamldebugger)
+
+allKeywords
+"plot" %in% allKeywords
+
+workdir = "/Users/Timm/Documents/Humboldt/5.Semester/SPL/Repository/spl-pirates/SOEPQ1"
+
+d_init = yaml.debugger.init(workdir, show_keywords = TRUE)
+
+qnames = yaml.debugger.get.qnames(d_init$RootPath)
+
+d_results = yaml.debugger.run(qnames, d_init)
+
+OverView = yaml.debugger.summary(qnames, d_results, summaryType = "mini")
+
 # Delete everything - Cleanup-Command - BE CAREFUL WITH IT!
 # rm(list=ls(all=TRUE))
   
