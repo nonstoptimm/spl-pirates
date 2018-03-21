f8d52fae801d94cdef5e8cb6607403c4805582b4
torsion214 <t_walz@gmx.net>
2018-03-21 14:57:57 +0100

    QuantletPreparation

--- Q0_QuantletPreparator.R
+++ Q0_QuantletPreparator.R
@@ -18,20 +18,20 @@ library(devtools)
 devtools::install_github("lborke/yamldebugger")
 
 # load the package every time you want to use 'yamldebugger'
-library(yamldebugger)
+# library(yamldebugger)
+# 
+# allKeywords
+# "plot" %in% allKeywords
 
-allKeywords
-"plot" %in% allKeywords
-
-workdir = "/Users/Timm/Documents/Humboldt/5.Semester/SPL/Repository/spl-pirates/SOEPQ1"
-
-d_init = yaml.debugger.init(workdir, show_keywords = TRUE)
-
-qnames = yaml.debugger.get.qnames(d_init$RootPath)
-
-d_results = yaml.debugger.run(qnames, d_init)
-
-OverView = yaml.debugger.summary(qnames, d_results, summaryType = "mini")
+# workdir = "/Users/Timm/Documents/Humboldt/5.Semester/SPL/Repository/spl-pirates/SOEPQ1"
+# 
+# d_init = yaml.debugger.init(workdir, show_keywords = TRUE)
+# 
+# qnames = yaml.debugger.get.qnames(d_init$RootPath)
+# 
+# d_results = yaml.debugger.run(qnames, d_init)
+# 
+# OverView = yaml.debugger.summary(qnames, d_results, summaryType = "mini")
 
 # Delete everything - Cleanup-Command - BE CAREFUL WITH IT!
 # rm(list=ls(all=TRUE))
