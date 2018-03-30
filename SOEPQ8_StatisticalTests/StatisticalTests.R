d3297951d04687faa85d07c001049253e3b736fe
torsion214 <t_walz@gmx.net>
2018-03-30 21:00:40 +0200

    Small Corrections

--- SOEPQ8_StatisticalTests/StatisticalTests.R
+++ SOEPQ8_StatisticalTests/StatisticalTests.R
@@ -4,9 +4,9 @@ library(lmtest)
 # Execution of Q1, Q4, Q5, Q6 and Q7 is necessary beforehand!
 
 # Create regressions fee and ree
-fee = plm(Log.Full.Employment.Rate ~ DiD.estimator.Kaitz + Log.Population + year13.dummy + year12.dummy, 
+fee = plm(Log.Full.Employment.Rate ~ DiD.estimator.Kaitz + Log.Population + year13.dummy + year12.dummy,
     data = estimation, index = c("State.of.Residence", "Wave"), model = "within")
-ree = plm(Log.Full.Employment.Rate ~ DiD.estimator.Kaitz + Log.Population + year13.dummy + year12.dummy, 
+ree = plm(Log.Full.Employment.Rate ~ DiD.estimator.Kaitz + Log.Population + year13.dummy + year12.dummy,
     data = estimation, index = c("State.of.Residence", "Wave"), model = "random")
 
 # Create function 'estimate_appropriate_model'
@@ -94,10 +94,10 @@ estimate_appropriate_model = function(fe, re) {
     return(output)
 }
 
-# Apply estimate_appropriate_model-function to the regressions in Q7 
+# Apply estimate_appropriate_model-function to the regressions in Q7
 # Use did_2.2.2 and specify counterpart random model
 fixed.1 = did_2.2.2
-random.1 = plm(Log.Part.Employment.Rate ~ DiD.estimator.Fraction + Log.Population + Interaction_Fraction_y13 + 
+random.1 = plm(Log.Part.Employment.Rate ~ DiD.estimator.Fraction + Log.Population + Interaction_Fraction_y13 +
     Interaction_Fraction_y12, data = estimation, index = c("State.of.Residence", "Wave"), model = "random")
 # Estimate the appropriate model
 estimate_appropriate_model(fixed.1, random.1)
@@ -106,7 +106,7 @@ summary(fixed.1)
 
 # Use did.4.2 and specify counterpart random model
 fixed.2 = did_4.2
-random.2 = plm(Delta.Log.Full.Employment ~ binary1, data = estimation, index = c("State.of.Residence", 
+random.2 = plm(Delta.Log.Full.Employment ~ binary1, data = estimation, index = c("State.of.Residence",
     "Wave"), model = "random")
 # Estimate the appropriate model
 estimate_appropriate_model(fixed.2, random.2)
